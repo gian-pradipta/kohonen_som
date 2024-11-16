2ac92bea1d27ee73781616c08ef2153cdfc95a49
Gian PG <gian.pradipta@ti.ukdw.ac.id>
2024-11-16 23:25:34 +0700

    kohonen SOM done

---
+++ main.py
@@ -0,0 +1,90 @@
+from typing import List
+import random
+
+def get_new_alpha(old_i : float):
+    return old_i * 0.5
+
+def hitung_jarak(weight : List[float], user_input : List[int]) -> float:
+    jarak = 0
+    for i in range(len(weight)):
+        jarak += (user_input[i] - weight[i])**2
+    jarak = jarak ** 0.5
+    return round(jarak, 2)
+
+def print_hitung_jarak(weight : List[float], user_input : List[int]) -> str:
+    hasil = ""
+    delimiter = "("
+    kurung = ""
+    for i in range(len(weight)):
+        kurung += f"{delimiter}{weight[i]} - {user_input[i]})^2"
+        delimiter = " + ("
+    kurung += ")"
+    hasil = f"sqrt({kurung}) = {hitung_jarak(weight, user_input)}"
+    return hasil
+
+def update_weight(weight : List[float], alpha : float, input : List[int]):
+    b = []
+    for i in range(len(weight)):
+        b.append(alpha * (input[i] - weight[i]))
+    for i in range(len(weight)):
+        weight[i] = round(weight[i] + b[i], 2)
+
+def print_updating_process(weight : List[float], alpha : float, input : List[int]) -> str:
+    hasil = ""
+    for i in range(len(weight)):
+        hasil += f"{i+1} = {weight[i]} + {alpha} * ({input[i]} - {weight[i]}) = {round(weight[i] + alpha * (input[i] - weight[i]), 2)}\n"
+    return hasil
+
+def main():
+    alpha : float = 0.6
+    w1 : List[float] = [0.4,0.2,0.1,0.1]
+    w2 : List[float] = [0.3,0.1,0.6,0.5]
+    inputs : List[int] = [
+        (1,1,0,0),
+        (0,0,0,1),
+        (1,0,0,0),
+        (0,0,0,1)
+    ]
+    for _ in range(1):
+        for i, inp in enumerate(inputs):
+            print()
+            print(f"\t\tIterasi-{i+1}".upper())
+            print()
+            print("Bobot Before")
+            print(f"W1 = {w1}")
+            print(f"W2 = {w2}")
+            print()
+            print("Jarak 1: ")
+            print(print_hitung_jarak(w1, inp))
+            jarak_w1 : float = hitung_jarak(w1, inp)
+            print()
+            print("Jarak 2: ")
+            print(print_hitung_jarak(w2, inp))
+            jarak_w2 : float = hitung_jarak(w2, inp)
+            new_alpha = get_new_alpha(alpha)
+            print(f"alpha yang baru = 0.5 * alpha lama = 0.5 * {alpha} = {new_alpha}")
+            alpha = new_alpha
+            if (jarak_w1 > jarak_w2):
+                print()
+                print("Karena jarak W2 lebih kecil dari W1, maka")
+                print("Update Bobot-2")
+                a = print_updating_process(w2, alpha, inp) 
+                print(a)
+                update_weight(w2, alpha, inp) 
+                
+            elif (jarak_w1 < jarak_w2):
+                print()
+                print("Karena jarak W1 lebih kecil dari W2, maka")
+                print("Update Bobot-1")
+                a = print_updating_process(w1, alpha, inp)
+                print(a)
+                update_weight(w1, alpha, inp)
+            
+            print()
+            print("Bobot After")
+            print(f"W1 = {w1}")
+            print(f"W2 = {w2}")
+            
+    
+if __name__ == "__main__":
+    main()
