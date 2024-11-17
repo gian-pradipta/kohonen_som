6258e025c8604be6a30c9c4e531e40bd4f0257fc
gian-pradipta <93311140+gian-pradipta@users.noreply.github.com>
2024-11-17 23:01:19 +0700

    Randomize initial weights

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def print_hitung_jarak(weight : List[float], user_input : List[int]) -> str:
     for i in range(len(weight)):
         kurung += f"{delimiter}{weight[i]} - {user_input[i]})^2"
         delimiter = " + ("
-    hasil = f"sqrt({kurung}) = {hitung_jarak(weight, user_input)}"
+    hasil = f"√({kurung}) = {hitung_jarak(weight, user_input)}"
     return hasil
 
 def update_weight(weight : List[float], alpha : float, input : List[int]) -> None:
@@ -36,54 +36,57 @@ def print_updating_process(weight : List[float], alpha : float, input : List[int
 
 def main():
     alpha : float = 0.6
-    w1 : List[float] = [0.4,0.2,0.1,0.1]
-    w2 : List[float] = [0.3,0.1,0.6,0.5]
+    w1 : List[float] = [round(random.random(),2) for _ in range(4)]
+    w2 : List[float] = [round(random.random(), 2) for _ in range(4)]
     inputs : List[int] = [
         (1,1,0,0),
         (0,0,0,1),
         (1,0,0,0),
         (0,0,0,1)
     ]
+    print(f"BOBOT AWAL")
+    print(f"W1 = {w1}")
+    print(f"W2 = {w2}")
     for _ in range(1):
         for i, inp in enumerate(inputs):
             print()
-            print(f"\t\tIterasi-{i+1}".upper())
+            print(f"Iterasi-{i+1}".upper())
             print()
             print("Bobot Before")
             print(f"W1 = {w1}")
             print(f"W2 = {w2}")
             print()
-            print("Jarak 1: ")
+            print("D1: ")
             print(print_hitung_jarak(w1, inp))
             jarak_w1 : float = hitung_jarak(w1, inp)
             print()
-            print("Jarak 2: ")
+            print("D2: ")
             print(print_hitung_jarak(w2, inp))
             jarak_w2 : float = hitung_jarak(w2, inp)
             new_alpha : float = get_new_alpha(alpha)
-            print(f"alpha yang baru = 0.5 * alpha lama = 0.5 * {alpha} = {new_alpha}")
+            print(f"α = 0.5 * α lama = 0.5 * {alpha} = {new_alpha}")
             alpha = new_alpha
             if (jarak_w1 > jarak_w2):
                 print()
-                print("Karena jarak W2 lebih kecil dari W1, maka")
-                print("Update Bobot-2")
+                
+                print("Update W2")
                 update_process_str : str = print_updating_process(w2, alpha, inp) 
                 print(update_process_str)
                 update_weight(w2, alpha, inp) 
                 
             elif (jarak_w1 < jarak_w2):
                 print()
-                print("Karena jarak W1 lebih kecil dari W2, maka")
-                print("Update Bobot-1")
+                
+                print("Update W1")
                 update_process_str = print_updating_process(w1, alpha, inp)
                 print(update_process_str)
                 update_weight(w1, alpha, inp)
             
             print()
-            print("Bobot After")
+            
             print(f"W1 = {w1}")
             print(f"W2 = {w2}")
             
     
-if __name__ == "__main__":
+if name == "main":
     main()
